dcf1b3015ebbff90ad8caccacf3cbe0bc3f31dd5
Donny Smith <d999ss@gmail.com>
2025-12-22 11:59:25 -0700

    Fix RSS feed syntax error - use template literals

--- app/feed/route.ts
+++ app/feed/route.ts
@@ -10,15 +10,31 @@ const SAMPLE_MEMES = [
 
 export async function GET() {
   const baseUrl = "https://insanitywolf.com"
-  
+
   const items = SAMPLE_MEMES.map((meme) => {
     const memeId = Buffer.from(JSON.stringify({ top: meme.top, bottom: meme.bottom })).toString("base64")
-    const memeUrl = baseUrl + "/meme/" + memeId
-    
-    return "<item><title><![CDATA[" + meme.top + " / " + meme.bottom + "]]></title><link>" + memeUrl + "</link><guid isPermaLink="true">" + memeUrl + "</guid><description><![CDATA[Insanity Wolf meme. Create your own at insanitywolf.com!]]></description><pubDate>" + meme.date.toUTCString() + "</pubDate></item>"
+    const memeUrl = `${baseUrl}/meme/${memeId}`
+
+    return `<item>
+      <title><![CDATA[${meme.top} / ${meme.bottom}]]></title>
+      <link>${memeUrl}</link>
+      <guid isPermaLink="true">${memeUrl}</guid>
+      <description><![CDATA[Insanity Wolf meme. Create your own at insanitywolf.com!]]></description>
+      <pubDate>${meme.date.toUTCString()}</pubDate>
+    </item>`
   }).join("")
 
-  const feed = "<?xml version="1.0" encoding="UTF-8"?><rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom"><channel><title>Insanity Wolf - Latest Memes</title><link>" + baseUrl + "</link><description>The most insane Insanity Wolf memes</description><language>en-us</language><lastBuildDate>" + new Date().toUTCString() + "</lastBuildDate>" + items + "</channel></rss>"
+  const feed = `<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+  <channel>
+    <title>Insanity Wolf - Latest Memes</title>
+    <link>${baseUrl}</link>
+    <description>The most insane Insanity Wolf memes</description>
+    <language>en-us</language>
+    <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
+    ${items}
+  </channel>
+</rss>`
 
   return new NextResponse(feed, {
     headers: {
